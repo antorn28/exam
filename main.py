4b660e157b8179c8ada1c2678a901360b23d8bec
maax <a@a.a>
2023-11-19 16:58:04 +0200

    001

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import openpyxl
 
 
+book = openpyxl.Workbook()
+sheet1 = book.active
 
-user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
+
+sheet1["A1"] = "Title"
+sheet1["B1"] = "Reviews"
+sheet1["C1"] = "Price"
+
+
+user = f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
 header = {"User-Agent": user}
 session = requests.Session()
 
 
 count = 2
 
-with open("tv.txt", "a", encoding="utf=8") as file:
-    for i in range(1,26):
-        print(f"page = {i}")
-        url = "https://allo.ua/ua/televizory/"
+
+with open("tvs.txt", "a", encoding="utf-8") as file:
+    for j in range(1, 26):
+        print(f"Page = {j}")
+        url = f"https://allo.ua/ua/televizory//p-={j}"
         response = session.get(url, headers=header)
 
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, features = "lxml")
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "lxml")
+            all_products = soup.find_all("div", class_="product-card")
 
-        all_products = soup.find_all(name = "div", class_="product-card")
 
-        print(len(all_products))
+            for product in all_products:
+                    title = product.find("a", class_="product-card__title")
+                    review = product.find("span", class_="comments-preview__count")
+                    price = product.find("span", class_="sum")
 
-        for product in all_products:
-            if product.find("div", class_="product-card"):
-                price = product.find("span", class_="product-card__title")
-                title = product.find("span", class_="v-pb__cur discount")
 
-                file.write(f"{title.text} {price.text}")
+                    print(title.text, review, price.text)
+                    file.write(f"{title.text} {review} {price.text}\n")
+                    sheet1[f"A{count}"] = title.text
+                    sheet1[f"B{count}"] = review
+                    sheet1[f"C{count}"] = price.text
+                    count += 1
+
+
+            book.save("tvs.xlsx")
+            book.close()
